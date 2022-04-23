fced794934ac8c7c27b0be4279dd14692a753a71
Marian <marianlopig@gmail.com>
2022-04-23 13:13:51 +0200

    main container created with innerHTML with all structure

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-new */
+import MainContainerComponent from "./components/MainContainerComponent.js";
+
 const getAllPokemon = async () => {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon");
   const pokemons = await response.json();
@@ -6,3 +9,6 @@ const getAllPokemon = async () => {
 (async () => {
   await getAllPokemon();
 })();
+
+const parentMainContainer = document.querySelector(".container");
+new MainContainerComponent(parentMainContainer);
